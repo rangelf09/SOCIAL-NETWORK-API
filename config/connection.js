26eb58c12b6a7aa50794f9085ff8872ad85028f1
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-11-28 15:47:30 -0600

    edit text in config

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkapi', {
-    userNewUrlParser: true,
-    userUnifiedTopology: true
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+module.exports = connection;
